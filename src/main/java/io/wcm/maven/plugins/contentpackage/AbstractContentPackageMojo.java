bb2dc3742e0d53c9a99e7ddaa1b1f26ec7fa63b8
sseifert <sseifert@pro-vision.de>
2014-10-06 17:36:36 +0200

    add "wcmio-" goal prefix

---
+++ src/main/java/io/wcm/maven/plugins/contentpackage/AbstractContentPackageMojo.java
@@ -0,0 +1,331 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.maven.plugins.contentpackage;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.regex.Pattern;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.CharEncoding;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Common functionality for all mojors
+ */
+abstract class AbstractContentPackageMojo extends AbstractMojo {
+
+  /**
+   * The Maven project.
+   */
+  @Parameter(property = "project", required = true, readonly = true)
+  private MavenProject project;
+
+  /**
+   * The name of the content package file to install on the target system.
+   * If not set, the primary artifact of the project is considered the content package to be installed.
+   */
+  @Parameter(property = "vault.file", defaultValue = "${project.build.directory}/${project.build.finalName}.zip")
+  private File packageFile;
+
+  /**
+   * The URL of the HTTP service API of the CRX package manager.
+   * See <a href=
+   * "http://dev.day.com/docs/en/crx/current/how_to/package_manager.html#Managing%20Packages%20on%20the%20Command%20Line"
+   * >HTTP service Interface</> for details on this interface.
+   */
+  @Parameter(property = "vault.serviceURL", required = true, defaultValue = "http://localhost:4502/crx/packmgr/service")
+  private String serviceURL;
+
+  /**
+   * The user name to authenticate as against the remote CRX system.
+   */
+  @Parameter(property = "vault.userId", required = true, defaultValue = "admin")
+  private String userId;
+
+  /**
+   * The password to authenticate against the remote CRX system.
+   */
+  @Parameter(property = "vault.password", required = true, defaultValue = "admin")
+  private String password;
+
+  /**
+   * Set this to "true" to skip installing packages to CRX although configured in the POM.
+   */
+  @Parameter(property = "vault.skip", defaultValue = "false")
+  private boolean skip;
+
+  /**
+   * Number of times to retry upload and install via CRX HTTP interface if it fails.
+   */
+  @Parameter(property = "vault.retryCount", defaultValue = "0")
+  private int retryCount;
+
+  /**
+   * Number of seconds between retry attempts.
+   */
+  @Parameter(property = "vault.retryDelay", defaultValue = "0")
+  private int retryDelay;
+
+  protected final MavenProject getProject() {
+    return this.project;
+  }
+
+  protected final File getPackageFile() {
+    return this.packageFile;
+  }
+
+  protected final String getCrxPackageManagerUrl() {
+    String serviceUrl = this.serviceURL;
+    // convert "legacy interface URL" with service.jsp to new CRX interface (since CRX 2.1)
+    serviceUrl = StringUtils.replace(serviceUrl, "/crx/packmgr/service.jsp", "/crx/packmgr/service");
+    // remove /.json suffix if present
+    serviceUrl = StringUtils.removeEnd(serviceUrl, "/.json");
+    return serviceUrl;
+  }
+
+  /**
+   * Set up http client with credentials
+   * @return Http client
+   * @throws MojoExecutionException
+   */
+  protected final HttpClient getCrxPackageManagerHttpClient() throws MojoExecutionException {
+    try {
+      URI crxUri = new URI(getCrxPackageManagerUrl());
+      HttpClient httpClient = new HttpClient();
+      httpClient.getParams().setAuthenticationPreemptive(true);
+      Credentials credentials = new UsernamePasswordCredentials(this.userId, this.password);
+      httpClient.getState().setCredentials(new AuthScope(crxUri.getHost(), crxUri.getPort(), AuthScope.ANY_REALM), credentials);
+      return httpClient;
+    }
+    catch (URISyntaxException ex) {
+      throw new MojoExecutionException("Invalid url: " + getCrxPackageManagerUrl(), ex);
+    }
+  }
+
+  /**
+   * Execute CRX HTTP Package manager method and parse/output xml response.
+   * @param httpClient Http client
+   * @param method Get or Post method
+   * @throws MojoExecutionException
+   */
+  protected final JSONObject executePackageManagerMethodJson(HttpClient httpClient, HttpMethodBase method,
+      int runCount) throws MojoExecutionException {
+
+    try {
+
+      String responseString = null;
+      try {
+        JSONObject response = null;
+
+        // execute method
+        int httpStatus = httpClient.executeMethod(method);
+        responseString = getResponseBodyAsString(method);
+        if (httpStatus == HttpStatus.SC_OK) {
+
+          // get response JSON
+          if (responseString != null) {
+            response = new JSONObject(responseString);
+          }
+          if (response == null) {
+            response = new JSONObject();
+            response.put("success", false);
+            response.put("msg", "Invalid response (null).");
+          }
+
+        }
+        else {
+          response = new JSONObject();
+          response.put("success", false);
+          response.put("msg", responseString);
+        }
+
+        return response;
+      }
+      catch (HttpException ex) {
+        throw new MojoExecutionException("Http method failed.", ex);
+      }
+      catch (IOException ex) {
+        throw new MojoExecutionException("Http method failed.", ex);
+      }
+      catch (JSONException ex) {
+        throw new MojoExecutionException("JSON operation failed:\n" + responseString, ex);
+      }
+      finally {
+        // cleanup
+        method.releaseConnection();
+      }
+
+    }
+    catch (MojoExecutionException ex) {
+      // retry again if configured so...
+      if (runCount < this.retryCount) {
+        getLog().info("ERROR: " + ex.getMessage());
+        getLog().debug("Package manager method execution failed.", ex);
+        getLog().info("---------------");
+
+        String msg = "Package manager method failed, try again (" + (runCount + 1) + "/" + this.retryCount + ")";
+        if (this.retryDelay > 0) {
+          msg += " after " + this.retryDelay + " second(s)";
+        }
+        msg += "...";
+        getLog().info(msg);
+        if (this.retryDelay > 0) {
+          try {
+            Thread.sleep(this.retryDelay * DateUtils.MILLIS_PER_SECOND);
+          }
+          catch (InterruptedException ex1) {
+            // ignore
+          }
+        }
+        return executePackageManagerMethodJson(httpClient, method, runCount + 1);
+      }
+      else {
+        throw ex;
+      }
+    }
+  }
+
+  /**
+   * Execute CRX HTTP Package manager method and parse/output xml response.
+   * @param httpClient Http client
+   * @param method Get or Post method
+   * @throws MojoExecutionException
+   */
+  protected final void executePackageManagerMethodHtml(HttpClient httpClient, HttpMethodBase method,
+      int runCount) throws MojoExecutionException {
+
+    try {
+
+      try {
+
+        // execute method
+        int httpStatus = httpClient.executeMethod(method);
+        if (httpStatus == HttpStatus.SC_OK) {
+
+          // get response xml
+          String response = getResponseBodyAsString(method);
+
+          // debug output whole xml
+          if (getLog().isDebugEnabled()) {
+            getLog().debug("CRX Package Manager Response:\n" + response);
+          }
+
+          // remove all HTML tags and special conctent
+          final Pattern HTML_STYLE = Pattern.compile("<style[^<>]*>[^<>]*</style>", Pattern.MULTILINE | Pattern.DOTALL);
+          final Pattern HTML_JAVASCRIPT = Pattern.compile("<script[^<>]*>[^<>]*</script>", Pattern.MULTILINE | Pattern.DOTALL);
+          final Pattern HTML_ANYTAG = Pattern.compile("<[^<>]*>");
+
+          response = HTML_STYLE.matcher(response).replaceAll("");
+          response = HTML_JAVASCRIPT.matcher(response).replaceAll("");
+          response = HTML_ANYTAG.matcher(response).replaceAll("");
+          response = StringUtils.replace(response, "&nbsp;", " ");
+
+          getLog().info(response);
+        }
+        else {
+          throw new MojoExecutionException("Failure:\n" + getResponseBodyAsString(method));
+        }
+
+      }
+      catch (HttpException ex) {
+        throw new MojoExecutionException("Http method failed.", ex);
+      }
+      catch (IOException ex) {
+        throw new MojoExecutionException("Http method failed.", ex);
+      }
+      finally {
+        // cleanup
+        method.releaseConnection();
+      }
+
+    }
+    catch (MojoExecutionException ex) {
+      // retry again if configured so...
+      if (runCount < this.retryCount) {
+        getLog().info("ERROR: " + ex.getMessage());
+        getLog().debug("Package manager method execution failed.", ex);
+        getLog().info("---------------");
+
+        String msg = "Package manager method failed, try again (" + (runCount + 1) + "/" + this.retryCount + ")";
+        if (this.retryDelay > 0) {
+          msg += " after " + this.retryDelay + " second(s)";
+        }
+        msg += "...";
+        getLog().info(msg);
+        if (this.retryDelay > 0) {
+          try {
+            Thread.sleep(this.retryDelay * DateUtils.MILLIS_PER_SECOND);
+          }
+          catch (InterruptedException ex1) {
+            // ignore
+          }
+        }
+        executePackageManagerMethodHtml(httpClient, method, runCount + 1);
+      }
+      else {
+        throw ex;
+      }
+    }
+  }
+
+  private String getResponseBodyAsString(HttpMethodBase method) throws MojoExecutionException {
+    InputStream is = null;
+    try {
+      is = method.getResponseBodyAsStream();
+      return IOUtils.toString(is, CharEncoding.UTF_8);
+    }
+    catch (IOException ex) {
+      throw new MojoExecutionException("Getting method response failed.", ex);
+    }
+    finally {
+      try {
+        if (is != null) {
+          is.close();
+        }
+      }
+      catch (IOException ex) {
+        // ignore
+      }
+    }
+  }
+
+  protected final boolean isSkip() {
+    return this.skip;
+  }
+
+}
---
+++ src/main/java/io/wcm/maven/plugins/contentpackage/InstallMojo.java
@@ -0,0 +1,207 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.maven.plugins.contentpackage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.repository.RepositorySystem;
+import org.json.JSONObject;
+
+/**
+ * Install a Content Package on a remote CRX or AEM system.
+ */
+@Mojo(name = "install", defaultPhase = LifecyclePhase.INSTALL, requiresProject = true,
+requiresDependencyResolution = ResolutionScope.RUNTIME, threadSafe = true)
+public class InstallMojo extends AbstractContentPackageMojo {
+
+  /**
+   * Whether to install (unpack) the uploaded package automatically or not.
+   */
+  @Parameter(property = "vault.install", defaultValue = "true")
+  private boolean install;
+
+  /**
+   * Force upload and install of content package. If set to false a package is not uploaded or installed
+   * if it was already uploaded before.
+   */
+  @Parameter(property = "vault.force", defaultValue = "true")
+  private boolean force;
+
+  /**
+   * The groupId of the artifact to install.
+   */
+  @Parameter(property = "vault.groupId")
+  private String groupId;
+
+  /**
+   * The artifactId of the artifact to install.
+   */
+  @Parameter(property = "vault.artifactId")
+  private String artifactId;
+
+  /**
+   * The packaging of the artifact to install.
+   */
+  @Parameter(property = "vault.packaging", defaultValue = "zip")
+  private String packaging;
+
+  /**
+   * The version of the artifact to install.
+   */
+  @Parameter(property = "vault.version")
+  private String version;
+
+  /**
+   * A string of the form <code>groupId:artifactId:version[:packaging]</code>.
+   */
+  @Parameter(property = "vault.artifact")
+  private String artifact;
+
+  @Component
+  private RepositorySystem repository;
+
+  /**
+   * Generates the ZIP.
+   * @throws MojoFailureException
+   */
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    File file = getArtifactFile();
+    if (file == null) {
+      file = getPackageFile();
+    }
+    installFile(file);
+  }
+
+  private File getArtifactFile() throws MojoFailureException, MojoExecutionException {
+    // check if artifact was specified
+    if ((StringUtils.isEmpty(this.artifactId) || StringUtils.isEmpty(this.groupId) || StringUtils.isEmpty(this.version))
+        || StringUtils.isEmpty(this.artifact)) {
+      return null;
+    }
+
+    // split up artifact string
+    if (StringUtils.isEmpty(this.artifactId)) {
+      String[] parts = StringUtils.split(this.artifact, ":");
+      if (parts.length < 3 && parts.length > 4) {
+        throw new MojoFailureException("Invalid artifact: " + artifact);
+      }
+      this.groupId = parts[0];
+      this.artifactId = parts[1];
+      this.version = parts[2];
+      if (parts.length > 3) {
+        this.packaging = parts[3];
+      }
+    }
+    Artifact artifactObject = repository.createArtifact(groupId, artifactId, version, packaging);
+    ArtifactResolutionRequest request = new ArtifactResolutionRequest();
+    request.setArtifact(artifactObject);
+    ArtifactResolutionResult result = repository.resolve(request);
+    if (result.isSuccess()) {
+      return artifactObject.getFile();
+    }
+    else {
+      throw new MojoExecutionException("Unable to download artifact: " + artifactObject.toString());
+    }
+  }
+
+  /**
+   * Deploy file via package manager
+   */
+  private void installFile(File file) throws MojoExecutionException {
+    if (isSkip()) {
+      return;
+    }
+
+    try {
+      if (this.install) {
+        getLog().info("Upload and install " + file.getName() + " to " + getCrxPackageManagerUrl());
+      }
+      else {
+        getLog().info("Upload " + file.getName() + " to " + getCrxPackageManagerUrl());
+      }
+
+      // setup http client with credentials
+      HttpClient httpClient = getCrxPackageManagerHttpClient();
+
+      // prepare post method
+      PostMethod post = new PostMethod(getCrxPackageManagerUrl() + "/.json?cmd=upload");
+      List<Part> parts = new ArrayList<Part>();
+      parts.add(new FilePart("package", file));
+      if (this.force) {
+        parts.add(new StringPart("force", "true"));
+      }
+      post.setRequestEntity(new MultipartRequestEntity(parts.toArray(new Part[parts.size()]), post.getParams()));
+
+      // execute post
+      JSONObject response = executePackageManagerMethodJson(httpClient, post, 0);
+      boolean success = response.optBoolean("success", false);
+      String msg = response.optString("msg", null);
+      String path = response.optString("path", null);
+
+      if (success) {
+
+        if (this.install) {
+          getLog().info("Package uploaded, now installing...");
+
+          post = new PostMethod(getCrxPackageManagerUrl() + "/console.html" + path + "?cmd=install");
+
+          // execute post
+          executePackageManagerMethodHtml(httpClient, post, 0);
+        }
+        else {
+          getLog().info("Package uploaded successfully (without installing).");
+        }
+
+      }
+      else if (StringUtils.startsWith(msg, "Package already exists") && !this.force) {
+        getLog().info("Package skipped because it was already uploaded.");
+      }
+      else {
+        throw new MojoExecutionException("Package upload failed: " + msg);
+      }
+
+    }
+    catch (FileNotFoundException ex) {
+      throw new MojoExecutionException("File not found: " + file.getAbsolutePath(), ex);
+    }
+  }
+
+}
