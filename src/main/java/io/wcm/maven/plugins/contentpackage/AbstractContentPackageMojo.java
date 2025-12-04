067df9b36c295026f8bd9fee1a83ed4526bd67dd
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-12-04 09:08:36 +0100

    CRX Package Manager Upload: Switch default URL for system ready check to /systemready.json

--- src/main/java/io/wcm/maven/plugins/contentpackage/AbstractContentPackageMojo.java
+++ src/main/java/io/wcm/maven/plugins/contentpackage/AbstractContentPackageMojo.java
@@ -201,7 +201,7 @@ abstract class AbstractContentPackageMojo extends AbstractMojo {
    * after installing finishing the upload. This works only for AEMaaCS SDK instances.
    *
    * <p>
-   * Expected is an URL like: http://localhost:4502/systemready
+   * Expected is an URL like: http://localhost:4502/systemready.json
    * </p>
    *
    * <p>
@@ -332,7 +332,7 @@ abstract class AbstractContentPackageMojo extends AbstractMojo {
     }
     // if not set use hostname from serviceURL and add default path to bundle status
     String baseUrl = VendorInstallerFactory.getBaseUrl(buildPackageManagerUrl());
-    return baseUrl + "/systemready";
+    return baseUrl + "/systemready.json";
   }
 
   private String buildPackageManagerInstallStatusUrl() throws MojoExecutionException {
