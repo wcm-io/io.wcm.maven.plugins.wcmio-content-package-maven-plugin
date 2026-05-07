4801974264de73907cb41894ebb55874a70b797b
Stefan Seifert <stefanseifert@users.noreply.github.com>
2026-05-07 12:29:40 +0200

    eliminate code warnings

--- src/main/java/io/wcm/maven/plugins/contentpackage/InstallMojo.java
+++ src/main/java/io/wcm/maven/plugins/contentpackage/InstallMojo.java
@@ -43,6 +43,7 @@ import io.wcm.tooling.commons.packmgr.install.PackageInstaller;
  */
 @Mojo(name = "install", defaultPhase = LifecyclePhase.INSTALL, requiresProject = false, requiresDependencyResolution = ResolutionScope.RUNTIME,
     threadSafe = true)
+@SuppressWarnings("java:S6813") // allow field injection
 public final class InstallMojo extends AbstractContentPackageMojo {
 
   /**
@@ -172,6 +173,7 @@ public final class InstallMojo extends AbstractContentPackageMojo {
   private List<RemoteRepository> repositories;
 
   @Override
+  @SuppressWarnings("java:S3776") // accept complexity
   public void execute() throws MojoExecutionException, MojoFailureException {
     if (isSkip()) {
       return;
